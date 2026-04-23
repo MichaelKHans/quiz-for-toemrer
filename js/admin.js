996cc4c8a5a4f0421d036506052db53cd0cd8eee
mhtegnestue <mail@mh-t.dk>
2026-04-23 22:49:46 +0200

    Enlarging PIN code visibility v4.9.6

--- js/admin.js
+++ js/admin.js
@@ -5,7 +5,7 @@
 
 import { saveDbToCloud, getDbFromCloud } from './firebase-service.js';
 
-const APP_VERSION = "v4.9.5";
+const APP_VERSION = "v4.9.6";
 const ADMIN_PASSWORD = "tømrer123";
 
 const UPDATE_LOG = [
